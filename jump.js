60453adb0a8c04fac27623b81a12aa1069a26c1a
Harry Zhang <zxh_126@qq.com>
2018-01-03 23:39:57 +0800

    jump cheat

---
+++ jump.js
@@ -0,0 +1,67 @@
+var CryptoJS = require('crypto-js');
+var http = require('http');
+var querystring = require('querystring');
+
+function encrypt (text, originKey) {
+    var originKey = originKey.slice(0, 16),
+        key = CryptoJS.enc.Utf8.parse(originKey),
+        iv = CryptoJS.enc.Utf8.parse(originKey),
+        msg = JSON.stringify(text)
+    var ciphertext = CryptoJS.AES.encrypt(msg, key, {
+        iv: iv,
+        mode: CryptoJS.mode.CBC,
+        padding: CryptoJS.pad.Pkcs7
+    });
+    return ciphertext.toString()
+}
+
+function decrypt (text, originKey) {
+    var originKey = originKey.slice(0, 16),
+        key = CryptoJS.enc.Utf8.parse(originKey),
+        iv = CryptoJS.enc.Utf8.parse(originKey)
+    var bytes = CryptoJS.AES.decrypt(text, key, {
+        iv: iv
+    })
+    var plaintext = CryptoJS.enc.Utf8.stringify(bytes)
+    return plaintext
+}
+
+
+module.exports = {
+  
+   *beforeSendRequest(requestDetail) {
+    if (requestDetail.url.indexOf('https://mp.weixin.qq.com/wxagame/wxagame_settlement') === 0) {
+	   var data=JSON.parse(requestDetail.requestData.toString());
+	   var sessionid=data['base_req']['session_id'];
+	   var actiondata=data['action_data'];
+	   actiondata=JSON.parse(decrypt(actiondata,sessionid));
+	   actiondata['score']=Math.round(8000+Math.random()*2000);
+	   
+	     var action = [],
+            musicList = [],
+            touchList = [];
+	
+		for(var i=Math.round(10000+Math.random()*2000);i>0;i--){
+				action.push([Math.random().toFixed(3),(Math.random()*2).toFixed(2),i/5000==0?true:false]);
+				musicList.push(false);
+				touchList.push([(250-Math.random()*10).toFixed(4),(670-Math.random()*20).toFixed(4)]);
+			}
+		actiondata['game_data']=JSON.stringify({
+                seed: Date.now(),
+                action: action,
+                musicList: musicList,
+                touchList: touchList,
+                version: 1
+            });
+	   console.log(actiondata);
+	   var senddata='{"base_req":{"session_id":"'+sessionid+'","fast":1},"action_data":"'+encrypt(actiondata, sessionid)+'"}';
+	   
+	   console.log(senddata);
+	   return {
+        requestData: senddata
+      };
+	   
+    }
+  },
+};
+
